47f50f8cd09ae17e6fd95de4345e17232dfae0f4
Edward Hesketh <inbox@edwardh.dev>
2025-03-14 19:57:26 +0000

    feat: use nix flake to build site

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Edward Hesketh's professional facing website, written to be as minimal as reasonable.";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+  outputs = { nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      zas = pkgs.buildGoModule {
+        pname = "zas";
+        version = "025861715cd4cd56ffa39bb961cb3bffe586a716";
+        src = pkgs.fetchFromGitHub {
+          owner = "darccio";
+          repo = "zas";
+          rev = "025861715cd4cd56ffa39bb961cb3bffe586a716";
+          hash = "sha256-k80xaaWLOyc9Br81C1oH0UvndxX3J0P+D/ONPjxO/4U=";
+        };
+        vendorHash = "sha256-3SPAc4X03aMckSs4hkVnT8ARTGV7gTudVvoe2OHX7PQ=";
+      };
+    in
+    rec {
+      packages.edwardh-dev = pkgs.stdenv.mkDerivation {
+        name = "edwardh-dev";
+        src = ./.;
+        buildInputs = [ zas ];
+        buildPhase = ''
+          zas
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -r .zas/deploy/. $out
+        '';
+      };
+      packages.default = packages.edwardh-dev;
+    };
+}
